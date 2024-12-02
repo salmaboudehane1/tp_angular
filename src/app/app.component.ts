39c475b31cabd8db62a86e883795d45660f5a375
salmaboudehane1 <salma.solo0204@gmail.com>
2024-12-02 14:23:59 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Book } from './shared/book.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule], // Import CommonModule here
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tp_angular';
+  books: Book[] = [
+    new Book ('Crime and Punishment', true),
+    new Book ('Harry Potter', true),
+    new Book ('Secret', false),
+  ];
 }
